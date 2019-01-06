0d11324396985ebbe9bc44163808d12db891c612
Nancyb <nancyeb27@yahoo.com>
2019-01-05 19:57:51 -0700

    saving files

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your port; if not 3306
-    port: 3306,
-
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "root",
-    database: "burgers;"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    showItemList();
-
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
+
+// Export connection for our ORM to use.
+module.exports = connection;
